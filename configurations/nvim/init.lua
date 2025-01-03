f93ca8d92ee2e29a2918bd3a4f4523fc1d14ffa3
Mikhail Safir <mikhail.safir@walmart.com>
2025-01-03 14:33:45 +0100

    Update configuration of nvim with hints and invisible chars

--- configurations/nvim/init.lua
+++ configurations/nvim/init.lua
@@ -1,5 +1,9 @@
 vim.o.clipboard = "unnamedplus"
 
+-- Show invisibles
+vim.o.list = true
+vim.o.listchars = "tab:>-,trail:~,extends:>,precedes:<,nbsp:+"
+
 -- Initialize Packer
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'  -- Packer manages itself
@@ -57,15 +61,62 @@ require('packer').startup(function(use)
             }
         end
     }
+
+    -- LSP Config
+    use {
+        'neovim/nvim-lspconfig',
+        config = function()
+            local lspconfig = require('lspconfig')
+
+            -- Configure SourceKit LSP for Swift
+            lspconfig.sourcekit.setup {
+                cmd = { "sourcekit-lsp" },
+                filetypes = { "swift", "objective-c", "objective-cpp" },
+                root_dir = lspconfig.util.root_pattern(".git", "Package.swift"),
+            }
+        end
+    }
+
+    -- Autocomplete (nvim-cmp)
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-vsnip',        -- Snippet support
+            'hrsh7th/vim-vsnip',
+        },
+        config = function()
+            local cmp = require'cmp'
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-Space>'] = cmp.mapping.complete(),              -- Trigger completion manually
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),   -- Confirm selection
+                    ['<Up>'] = cmp.mapping.select_prev_item(),           -- Navigate up
+                    ['<Down>'] = cmp.mapping.select_next_item(),         -- Navigate down
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),          -- Navigate up (alternative)
+                    ['<C-n>'] = cmp.mapping.select_next_item(),          -- Navigate down (alternative)
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                },
+            }
+        end
+    }
 end)
 
 -- Keybindings
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
-
--- Optional: Remove Redundant Terminal Mapping
--- This is handled by ToggleTerm's `open_mapping`.
--- vim.api.nvim_set_keymap('n', '<C-c>', ':split | terminal<CR>', { noremap = true, silent = true })
 
 -- Remap delete commands to avoid affecting the system clipboard
 vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true, silent = true })
